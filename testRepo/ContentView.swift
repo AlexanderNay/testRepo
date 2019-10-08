7a96465ba48cf33e15027c032c62a61767fde9a9
Alexander Nay <naymushinas56@gmail.com>
2019-10-08 17:45:17 +0300

    test

---
+++ testRepo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  testRepo
+//
+//  Created by Alexander Nay on 10/8/19.
+//  Copyright © 2019 Alexander Nay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        //test
+    }
+}
